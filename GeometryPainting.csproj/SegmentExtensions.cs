4c5399e0dfff716bf4fc396dfd6fec47c44a5f57
Kirill <kirillaromad@mail.ru>
2020-10-27 23:54:13 +0300

    GeometryPainting

---
+++ GeometryPainting.csproj/SegmentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryTasks;
+using System.Drawing;
+
+namespace GeometryPainting
+{
+    //Напишите здесь код, который заставит работать методы segment.GetColor и segment.SetColor
+    public static class SegmentExtensions
+    {
+        public static  Dictionary<Segment, Color> ColorsDictionary = new Dictionary<Segment, Color>();
+
+        public static Color GetColor(this Segment segment)
+        {
+            if (!ColorsDictionary.ContainsKey(segment))
+                return Color.Black;
+            return ColorsDictionary[segment];
+        }
+
+        public static void SetColor(this Segment segment, Color color)
+        {
+            ColorsDictionary[segment] = color;
+        }
+    }
+}
