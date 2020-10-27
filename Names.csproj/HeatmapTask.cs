d25d0151f8e2fcc094bd7a96f66818553a5f7afb
Kirill <kirillaromad@mail.ru>
2020-10-28 00:07:02 +0300

    Names

---
+++ Names.csproj/HeatmapTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var xLabels = new string[30];
+            for (int i = 0; i < 30; i++)
+                xLabels[i] = (i + 2).ToString();
+
+            var yLabels = new string[12];
+            for (int i = 0; i < 12; i++)
+                yLabels[i] = (i + 1).ToString();
+
+            var heat = new double[30, 12];
+            foreach (var name in names)
+                if (name.BirthDate.Day != 1)
+                {
+                    heat[name.BirthDate.Day - 2, name.BirthDate.Month - 1]++;
+                }
+
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                heat,
+                xLabels,
+                yLabels);
+        }
+    }
+}
---
+++ Names.csproj/HistogramTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var days = new string[31];
+            for (int i = 0; i < days.Length; i++)
+                days[i] = (i + 1).ToString();      
+
+            var birthsCounts = new double[31];
+            foreach (var n in names)
+                if (n.Name == name && n.BirthDate.Day != 1)
+                    birthsCounts[n.BirthDate.Day - 1]++;
+
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name),
+                days,
+                birthsCounts);
+        }
+    }
+}
