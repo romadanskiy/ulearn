34ee15dc15915b2cd900ea19630c88e7748d3b4c
Kirill <kirillaromad@mail.ru>
2020-10-28 00:04:18 +0300

    LimitedSizeStack

---
+++ LimitedSizeStack.csproj/LimitedSizeStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        private readonly int size;
+        private readonly LinkedList<T> list = new LinkedList<T>();
+        public LimitedSizeStack(int limit)
+        {
+            size = limit;
+        }
+
+        public void Push(T item)
+        {
+            if (list.Count == size)
+            {
+                list.RemoveFirst();
+            }
+            list.AddLast(item);
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0) throw new InvalidOperationException();
+            var result = list.Last;
+            list.RemoveLast();
+            return result.Value;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+    }
+}
---
+++ LimitedSizeStack.csproj/ListModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    enum Operation { Add, Remove }
+
+    public class ListModel<TItem>
+    {
+        private class HistoryItem
+        {
+            public TItem Item;
+            public int Index;
+            public Operation Operation;
+        }
+        private readonly LimitedSizeStack<HistoryItem> history;
+        public List<TItem> Items { get; }
+        public int Limit;
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            Limit = limit;
+            history = new LimitedSizeStack<HistoryItem>(limit);
+        }
+
+        public void AddItem(TItem item)
+        {
+            Items.Add(item);
+            history.Push(new HistoryItem { Item = item, Index = Items.Count - 1, Operation = Operation.Add });
+        }
+
+        public void RemoveItem(int index)
+        {
+            history.Push(new HistoryItem { Item = Items[index], Index = index, Operation = Operation.Remove });
+            Items.RemoveAt(index);
+        }
+
+        public bool CanUndo()
+        {
+            return history.Count != 0;
+        }
+
+        public void Undo()
+        {
+            if (CanUndo())
+            {
+                var historyItem = history.Pop();
+                if (historyItem.Operation == Operation.Remove)
+                    Items.Insert(historyItem.Index, historyItem.Item);
+                else
+                    Items.RemoveAt(historyItem.Index);
+            }
+        }
+    }
+}
