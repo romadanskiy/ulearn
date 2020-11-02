2624298cd1130631b1e9549ef1b2876297b90aa7
Kirill <kirillaromad@mail.ru>
2020-11-02 20:23:49 +0300

    Reflection

---
+++ Reflection.Differentiation.csproj/Algebra.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection.Differentiation
+{
+   public static class Algebra
+   {
+      public static Expression<Func<double, double>> Differentiate(Expression<Func<double, double>> func)
+      {
+         var exp = GetExpression(func.Body);
+         return (Expression<Func<double, double>>) Expression.Lambda(exp, func.Parameters);
+      }
+
+      private static Expression GetExpression(Expression body)
+      {
+         switch (body)
+         {
+            // константа
+            case ConstantExpression _:
+               return Expression.Constant(0.0);
+            
+            // параметр
+            case ParameterExpression _:
+               return Expression.Constant(1.0);
+            
+            // операция
+            case BinaryExpression operation:
+               var left = operation.Left;
+               var right = operation.Right;
+               switch (operation.NodeType)
+               {
+                  case ExpressionType.Add:
+                     return Expression.Add(GetExpression(left), GetExpression(right));
+                  case ExpressionType.Multiply:
+                     return Expression.Add(
+                        Expression.Multiply(GetExpression(left), right),
+                        Expression.Multiply(left, GetExpression(right))
+                     );
+                  default:
+                     throw new ArgumentException($"{operation.NodeType} is not supported operation");
+               }
+               
+            // метод
+            case MethodCallExpression method:
+               var parameter = method.Arguments[0];
+               Expression exp;
+               switch (method.Method.Name)
+               {
+                  case "Sin":
+                     exp = Expression.Call(typeof(Math).GetMethod("Cos", new[] { typeof(double) }), parameter);
+                     break;
+                  case "Cos":
+                     exp = Expression.Multiply(
+                        Expression.Constant(-1.0),
+                        Expression.Call(typeof(Math).GetMethod("Sin", new[] { typeof(double) }), parameter)
+                        );
+                     break;
+                  default:
+                     throw new ArgumentException($"{method.Method.Name} is not supported method");
+               }
+               return Expression.Multiply(GetExpression(parameter), exp);
+            
+            default:
+               throw new ArgumentException($"{body} is not supported syntax");
+         }
+      }
+   }
+}
---
+++ Reflection.Randomness.csproj/Generator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Reflection.Randomness
+{
+    public class Generator<T>
+    {
+        private static readonly PropertyInfo[] properties;
+        private static readonly FromDistribution[] distributions;
+
+        static Generator()
+        {
+            properties = typeof(T)
+                .GetProperties()
+                .Where(p => p.GetCustomAttributes(typeof(FromDistribution), false).Length > 0)
+                .ToArray();
+
+            distributions = properties
+                .Select(p => (FromDistribution) Attribute.GetCustomAttribute(p, typeof(FromDistribution)))
+                .ToArray();
+        }
+
+        public T Generate(Random rnd)
+        {
+            var t = Activator.CreateInstance(typeof(T));
+
+            for (var i = 0; i < properties.Length; i++)
+            {
+                var d = distributions[i];
+                CheckDistributionType(d);
+                var continuousDistribution = GetContinuousDistribution(d);
+                properties[i].SetValue(t, continuousDistribution.Generate(rnd));
+            }
+
+            return (T)t;
+        }
+
+        private static void CheckDistributionType(FromDistribution d)
+        {
+            if (!d.DistributionType.GetInterfaces().Contains(typeof(IContinuousDistribution)))
+                throw new ArgumentException($"{d.DistributionType.Name} is not a DistributionType");
+        }
+
+        private static IContinuousDistribution GetContinuousDistribution(FromDistribution d)
+        {
+            try
+            {
+               return (IContinuousDistribution) Activator.CreateInstance(d.DistributionType, d.Parameters);
+            }
+            catch
+            {
+                var type = d.DistributionType.Name;
+                var n = d.Parameters.Length;
+                throw new ArgumentException($"{type} doesn't have a constructor with {n} number of arguments");
+            }
+        }
+    }
+    
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FromDistribution : Attribute
+    {
+        public readonly Type DistributionType;
+        public readonly object[] Parameters;
+
+        public FromDistribution(Type distributionType, params object[] parameters)
+        {
+            DistributionType = distributionType;
+            Parameters = parameters;
+        }
+    }
+}
